a014544b91e19a35165ee191f6bc70ebb6598d5c
nio <niopeng@hotmail.com>
2020-10-21 21:42:07 -0700

    add license

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+'''
+Code for Fast k-Nearest Neighbour Search via Prioritized DCI
+
+This code implements the method described in the Prioritized DCI paper,
+which can be found at https://arxiv.org/abs/1703.00440
+
+This file is a part of the Dynamic Continuous Indexing reference
+implementation.
+
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+Copyright (C) 2020    Ke Li, Shichong Peng
+'''
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -1,8 +1,18 @@
 /*
  * Code for Fast k-Nearest Neighbour Search via Prioritized DCI
  *
- * This code implements the method described by Li et al., which can be found at https://arxiv.org/abs/1703.00440
- * This code also builds off of code written by Ke Li.
+ * This code implements the method described in the Prioritized DCI paper,
+ * which can be found at https://arxiv.org/abs/1703.00440
+ *
+ * This file is a part of the Dynamic Continuous Indexing reference
+ * implementation.
+ *
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (C) 2020    Ke Li, Shichong Peng
  */
 
 #include <malloc.h>
