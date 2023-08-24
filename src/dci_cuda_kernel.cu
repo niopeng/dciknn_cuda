b66a1fda4379657c2a6d2e5bbb504cbed04386f2
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-08-24 01:18:13 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -614,15 +614,23 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 
 	int head = (int) (threadIdx.x / thread_per_head);
 	int curr_start = head * thread_per_head;
-
-	// test
-	if (blockIdx.x == 0) {
-		if (threadIdx.x == 0) {
-			printf("h = %d | ", head);
-			printf("s = %d | ", curr_start);
-			printf("b = %d\n", blockIdx.x);
+	if (gridDim.x == 0) {
+		if (blockIdx.x == 0) {
+			if (threadIdx.x == 0) {
+				printf("thread_per_head: %d/n", thread_per_head);
+			}
 		}
 	}
+
+	// test
+	//if (blockIdx.x == 0) {
+	//	if (threadIdx.x == 0) {
+	//		printf("head = %d | ", head);
+	//		printf("s = %d | ", curr_start);
+	//		printf("b = %d\n", blockIdx.x);
+	//		printf("tid = %d | ", threadIdx.x);
+	//	}
+	//}
 	// test
 
 	int points_per_block = (dci_inst->num_points + gridDim.x - 1) / gridDim.x;
