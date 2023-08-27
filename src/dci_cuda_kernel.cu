cb074b28a9c39cd533007b51df321c2115024ffa
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-08-27 16:48:52 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -681,8 +681,11 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 					//printf("head: %d\n", ch);
 					for (int ni = 0; ni < num_indices; ni++) {
 						printf("%d ", left_pos[ch * num_indices + ni]);
+						printf("\n");
 						printf("%d ", right_pos[ch * num_indices + ni]);
+						printf("\n");
 						printf("%d ", cur_pos[ch * num_indices + ni]);
+						printf("\n");
 						printf("%f ", index_priority[ch * num_indices + ni]);
 					}
 					printf("\n");
