72e2dc0c0de77ac150835d290b8f47e83b74324a
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-08-27 02:19:21 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -659,8 +659,8 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 
 		//for (int b = 0; b < block_size; b++) {
 		if (blockIdx.x == 0) {
-			printf("block: %d\n", blockIdx.x);
 			if (threadIdx.x == 0) {
+				printf("block: %d\n", blockIdx.x);
 				for (int ch = 0; ch < curr_head; ch++) {
 					//printf("head: %d\n", ch);
 					for (int ni = 0; ni < num_indices; ni++) {
