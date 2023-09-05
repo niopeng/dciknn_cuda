4f3d8ae68c453f40e089b93669e91313b8444ace
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-09-05 03:50:59 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -884,11 +884,11 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 						position[curr_head] = cur_pos[i[curr_head]]; // position already adjust on current head
 					}
 
-					if (blockIdx.x == 0) {
-						if (threadIdx.x == 0) {
-							printf("i: %d | position: %d\n", i[curr_head], position[curr_head]);
-						}
-					}
+					//if (blockIdx.x == 0) {
+					//	if (threadIdx.x == 0) {
+					//		printf("i: %d | position: %d\n", i[curr_head], position[curr_head]);
+					//	}
+					//}
 
 					__syncthreads();
 					//int cur_index = position[curr_head] + threadIdx.x;
@@ -897,11 +897,11 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 					// this also mean it now process less number of index but work on multiple head
 					int cur_index = position[curr_head] + head_threadIdx;
 	
-					if (blockIdx.x == 0) {
-						if (threadIdx.x == 0) {
-							printf("cur_index: %d\n", cur_index);
-						}
-					}
+					//if (blockIdx.x == 0) {
+					//	if (threadIdx.x == 0) {
+					//		printf("cur_index: %d\n", cur_index);
+					//	}
+					//}
 
 					if (cur_index >= 0 && cur_index < num_points_in_block) {
 						int cur_point = dci_inst->indices[cur_index
@@ -909,6 +909,16 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 								+ blockIdx.x * points_per_block].value; // value is position within each head, max is num_points
 						counts[cur_point + dci_inst->num_points * m[curr_head]
 								+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head]++;
+
+						if (blockIdx.x == 0) {
+							if (threadIdx.x == 0) {
+								printf("cur_point: %d\n", cur_point);
+								printf("index key: %f\n", dci_inst->indices[cur_index + dci_inst->num_points * i[curr_head] + blockIdx.x * points_per_block].key);
+								printf("index value: %d\n", dci_inst->indices[cur_index + dci_inst->num_points * i[curr_head] + blockIdx.x * points_per_block].value);
+								printf("count: %d\n", counts[cur_point + dci_inst->num_points * m[curr_head] + dci_inst->num_comp_indices * dci_inst->num_points * curr_head]);
+							}
+						}
+
 						if (counts[cur_point + dci_inst->num_points * m[curr_head]
 								+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head]
 								== dci_inst->num_simp_indices) { 
