9811df54f212c8b418cba342eb9a14495afa7dad
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-09-11 20:04:13 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -951,6 +951,7 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 							//}
 						}
 
+						/*
 						if (blockIdx.x == 0) {
 							if (threadIdx.x == 0) {
 								printf("\n");
@@ -961,10 +962,23 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 								printf("top_index_priority: %f\n", top_index_priority[curr_head]);
 							}
 						}
+						*/
 					}
 				}
 
 				__syncthreads();
+
+				if (blockIdx.x == 0) {
+					if (threadIdx.x == 0) {
+						
+						for (int tmp = 0; tmp < num_heads; tmp++) {
+							printf("\n");
+							printf("head: %d\n", tmp);
+							printf("top_h: %d\n", top_h[tmp]);
+							printf("top_h: %f\n", top_index_priority[tmp]);
+						}
+					}
+				}
 
 				// -----------------------------------------------------------------------------------------------
 				// Start working
