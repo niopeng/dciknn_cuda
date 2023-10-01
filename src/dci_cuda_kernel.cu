3eb78e7f382e962f75f5c8a6cd94f81a9591d85a
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-09-30 21:23:43 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -1786,9 +1786,9 @@ void dci_query(dci* const dci_inst, const int dim, const int num_heads, const in
 
 		cudaDeviceSynchronize();
 
-		//int data_total, data_size;
-		//float* h_data;
-		//int * i_data;
+		int data_total, data_size;
+		float* h_data;
+		int * i_data;
 
 		dci_query_single_point_by_block<<<block_size, thread_size>>>(
 				dci_inst,
@@ -1907,6 +1907,7 @@ void dci_query(dci* const dci_inst, const int dim, const int num_heads, const in
 
 		// -------- original result --------
 		
+		/*
 		// need to refresh the result holder to avoid carry over results
 		init_dist<<<block_size, thread_size>>>(d_top_candidates_dist,
 				num_neighbours * block_size * thread_size * num_heads, DBL_MAX);
@@ -2026,6 +2027,7 @@ void dci_query(dci* const dci_inst, const int dim, const int num_heads, const in
 		//		&(query_proj[j * num_indices]), *d_query_config,
 		//		d_top_candidates_dist, d_top_candidates_index, d_all_candidates,
 		//		counts, candidate_dists);
+		*/
 
 		break;
 
