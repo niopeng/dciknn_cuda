c0534502a744b3df3d18e5ac2ad9a0f7d4ef9235
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-10-30 00:55:03 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -2149,12 +2149,12 @@ void dci_query(dci* const dci_inst, const int dim, const int num_heads, const in
 
 		// candidate_dists
 
-		/*
 		data_total = dci_inst->num_points * num_heads;
 		data_size = sizeof(float) * data_total;
 		h_data = (float *) malloc(data_size);
 		cudaMemcpy(h_data, candidate_dists, data_size, cudaMemcpyDeviceToHost);
 
+		/*
 		if (j == 0) {
 			data_total = dci_inst->num_points * num_heads;
 			data_size = sizeof(float) * data_total;
