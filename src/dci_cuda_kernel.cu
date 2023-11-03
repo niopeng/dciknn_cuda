1ac191649274f1d0dfd66733c4d3250592093a3a
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-02 22:46:17 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -1687,7 +1687,8 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 
 		if (blockIdx.x == 0) {
 			if (threadIdx.x == 0) {
-				printf("\ntmp_count1 = %d | tmp_count2 = %d | tmp_count3 = %d\n", tmp_count1, tmp_count2, tmp_count3);
+				printf("\n");
+				printf("tmp_count1 = %d | tmp_count2 = %d | tmp_count3 = %d\n", tmp_count1, tmp_count2, tmp_count3);
 			}
 		}
 
