9504524187524b2ae5d17c0e20ec4941081d8e25
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-02 18:25:06 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -668,8 +668,8 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 	int num_indices = dci_inst->num_comp_indices * dci_inst->num_simp_indices;
 	int num_heads = dci_inst->num_heads;
 
-	__shared__ int *tmp_count1;
-	__shared__ int *tmp_count2;
+	__shared__ int tmp_count1;
+	__shared__ int tmp_count2;
 
 	// shared value is an array, each value in the array is correspond to a head
 	// the array size is num_heads
