e470afcea75189f8d9ba4e93392d7e9ccd4e38c1
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-06 23:29:13 -0800

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -1012,17 +1012,6 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 						// find the actual index position (complex indices * simple indices), adjust based on current head
 						i[curr_head] = top_h[curr_head] + m[curr_head] * dci_inst->num_simp_indices + curr_head * num_indices;
 						position[curr_head] = cur_pos[i[curr_head]]; // position already adjust on current head
-
-						//if (blockIdx.x == 0) {
-						//	if (threadIdx.x == 0) {
-						//		printf("\n");
-						//		printf("curr_head: %d\n", curr_head);
-						//		printf("top_h: %d\n", top_h[curr_head]);
-						//		printf("m: %d\n", m[curr_head]);
-						//		printf("position: %d\n", cur_pos[i[curr_head]]);
-						//		printf("\n");
-						//	}
-						//}
 					}
 
 					__syncthreads();
@@ -1058,7 +1047,7 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 						counts[cur_point + dci_inst->num_points * m[curr_head]
 								+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head]++;
 
-						//printf("%d ", cur_point + dci_inst->num_points * m[curr_head] + dci_inst->num_comp_indices * dci_inst->num_points * curr_head);
+						printf("%d ", cur_point + dci_inst->num_points * m[curr_head] + dci_inst->num_comp_indices * dci_inst->num_points * curr_head);
 
 						// possible issue 2
 						//int cur_index = position[curr_head] + head_threadIdx;
