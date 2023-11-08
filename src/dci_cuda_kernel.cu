19b960da870218732a396c278765926d23817fac
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-07 23:33:11 -0800

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -1765,7 +1765,7 @@ void dci_query(dci* const dci_inst, const int dim, const int num_heads, const in
 			cudaFree(i_data);
 
 			// d_top_candidates_dist
-
+			/*
 			data_total = num_neighbours * block_size * thread_size * num_heads;
 			data_size = sizeof(float) * data_total;
 			h_data = (float *) malloc(data_size);
@@ -1819,6 +1819,7 @@ void dci_query(dci* const dci_inst, const int dim, const int num_heads, const in
 			}
 			printf("\n");
 			cudaFree(i_data);
+			*/
 		}
 
 		// -------- original result --------
