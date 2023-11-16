9284b4379855eddbf57470af875d7d82caa18db8
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-15 21:44:47 -0800

    update

--- src/dci_cuda.cpp
+++ src/dci_cuda.cpp
@@ -106,8 +106,8 @@ torch::Tensor py_dci_query(py::handle py_dci_inst_wrapper, const int dim, const 
     int*  final_outputs;
     float* final_distances;
     const int output_size = num_neighbours * num_queries * num_heads;
-    cudaMalloc((void **) &(final_outputs), sizeof(int) * output_size);
-    cudaMalloc((void **) &(final_distances), sizeof(float) * output_size);
+    //cudaMalloc((void **) &(final_outputs), sizeof(int) * output_size);
+    //cudaMalloc((void **) &(final_distances), sizeof(float) * output_size);
 
     //printf("output size: %d\n", output_size);
 
