41f6ef48d4cf8e3f8c777397ffcb54c01b2080ba
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-15 22:00:54 -0800

    Update example.py

--- example.py
+++ example.py
@@ -110,16 +110,17 @@ def main():
         #print("Query 1:", query[0, :, :])
         #print("Query 2:", query[1, :, :])
 
-        #print(data.shape)
-        #print(query.shape)
+        print(data.shape)
+        print(query.shape)
 
         a = datetime.datetime.now()
         dci_db = DCI(dim, num_heads, num_comp_indices, num_simp_indices, block_size, thread_size, device=0)
         #dci_db = DCI(dim, 2, num_comp_indices, num_simp_indices, block_size, thread_size, device=0)
 
-        dci_db.add(data)
+        #dci_db.add(data)
+        
         ## Query
-        dci_db.query(query, num_neighbours, num_outer_iterations)
+        #dci_db.query(query, num_neighbours, num_outer_iterations)
         #indices, dists = dci_db.query(query, num_neighbours, num_outer_iterations)
         #torch.set_printoptions(threshold=10000)
         #print("Nearest Indices:", indices)
