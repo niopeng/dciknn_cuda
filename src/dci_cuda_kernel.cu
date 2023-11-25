36f853a856d269089431db655dd786a04ac08149
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-25 02:04:29 -0800

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -1017,18 +1017,18 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 									cur_dist = candidate_dists[cur_point + dci_inst->num_points * curr_head];
 								}
 							}
-
-							//if (blockIdx.x == 0) {
-							if (counts[cur_point + dci_inst->num_points * m
-							+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head] > dci_inst->num_simp_indices) {
-								printf("num_candidates = %d | count = %d | count_id = %d\n", num_candidates, 
-									counts[cur_point + dci_inst->num_points * m 
-										+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head], 
-									cur_point + dci_inst->num_points * m 
-										+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head);
-							}
-							//}
 						}
+
+						//if (blockIdx.x == 0) {
+						if (counts[cur_point + dci_inst->num_points * m
+							+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head] > dci_inst->num_simp_indices) {
+							printf("num_candidates = %d | count = %d | count_id = %d\n", num_candidates, 
+								counts[cur_point + dci_inst->num_points * m 
+									+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head], 
+								cur_point + dci_inst->num_points * m 
+									+ dci_inst->num_comp_indices * dci_inst->num_points * curr_head);
+						}
+						//}
 					}
 				}
 
