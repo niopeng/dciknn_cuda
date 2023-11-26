606cc83b57970d6e79c5d9890aa9c125bc7e1fb8
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-26 01:33:26 -0800

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -994,17 +994,18 @@ static void dci_query_single_point_by_block(const dci* const dci_inst,
 									num_candidates++;		
 								} else {
 									// Compute distance
+
 									cur_dist = compute_dist_device(
 											&(dci_inst->data[cur_point * dci_inst->dim
-													+ dci_inst->num_points * num_indices * curr_head]), 
+													+ dci_inst->num_points * dci_inst->dim * curr_head]), 
 											&(query[dci_inst->dim * num_queries * curr_head]), dci_inst->dim);
 
 									if (curr_head == 0) {
 										printf("cur_dist = %f | cur_point = %d | data = %f | data_index = %d | query = %f | query_index = %d\n", 
 											cur_dist, 
 											cur_point,
-											dci_inst->data[cur_point * dci_inst->dim + dci_inst->num_points * num_indices * curr_head],
-											cur_point * dci_inst->dim + dci_inst->num_points * num_indices * curr_head,
+											dci_inst->data[cur_point * dci_inst->dim + dci_inst->num_points * dci_inst->dim * curr_head],
+											cur_point * dci_inst->dim + dci_inst->num_points * dci_inst->dim * curr_head,
 											query[dci_inst->dim * num_queries * curr_head],
 											dci_inst->dim * num_queries * curr_head
 										);
