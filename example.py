afb13d030ebe52c0a09dd767660c5b22ea916c79
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-11-29 02:50:56 -0800

    Update example.py

--- example.py
+++ example.py
@@ -77,7 +77,7 @@ def main():
     #num_simp_indices = 10
     #num_outer_iterations = 80
     block_size = 100
-    thread_size = 10
+    thread_size = 20
     num_comp_indices = 2
     num_simp_indices = 10
     #num_outer_iterations = 5000
