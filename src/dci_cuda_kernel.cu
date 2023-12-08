7badb80de0fb4064da4dd8538016da5e4b1f3f80
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-12-07 19:23:16 -0800

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -215,8 +215,9 @@ void dci_add(dci* const dci_inst, const int dim, const int num_points, const int
 	dci_inst->num_points = num_points;
 
 	printf("num_points = %d\n", dci_inst->num_points);
-	//printf("num_points = %f\n", dci_inst->data[0]);
-	//printf("num_points = %f\n", dci_inst->data[1]);
+	printf("num_points = %f\n", dci_inst->data[0]);
+	printf("num_points = %f\n", dci_inst->data[1]);
+	printf("num_points = %f\n", dci_inst->data[2]);
 
 	/*
 	printf("h = 0\n");
@@ -237,6 +238,7 @@ void dci_add(dci* const dci_inst, const int dim, const int num_points, const int
 	printf("\n");
 	*/
 
+	/*
 	printf("h = 0\n");
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < dim; j++) {
@@ -244,6 +246,7 @@ void dci_add(dci* const dci_inst, const int dim, const int num_points, const int
 		}
 	}
 	printf("\n");
+	*/
 
 	/*
 	int data_size = sizeof(float) * num_points * dim * num_heads;
