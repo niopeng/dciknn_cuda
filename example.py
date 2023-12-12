b683555904758fbd27cb88475bf0fed58940de24
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-12-11 17:04:19 -0800

    update

--- example.py
+++ example.py
@@ -44,7 +44,7 @@ def main():
     #num_queries = 500
     num_pts = 500
     num_queries = 100
-    num_heads = 1
+    num_heads = 2
     
     data_and_queries = gen_data(dim, intrinsic_dim, num_pts + num_queries, num_heads)
 
