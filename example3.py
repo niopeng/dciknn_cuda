ef404864a2a627b6a5b8d2a4c0d690cba82cb02c
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-12-15 16:52:20 -0800

    Update example3.py

--- example3.py
+++ example3.py
@@ -84,10 +84,10 @@ def main():
 
         dci_db.add(data)
         # Query
-        #indices, dists = dci_db.query(query, num_neighbours, num_outer_iterations)
-        dci_db.query(query, num_neighbours, num_outer_iterations)
-        #print("Nearest Indices:", indices)
-        #print("Indices Distances:", dists)
+        indices, dists = dci_db.query(query, num_neighbours, num_outer_iterations)
+        #dci_db.query(query, num_neighbours, num_outer_iterations)
+        print("Nearest Indices:", indices)
+        print("Indices Distances:", dists)
         dci_db.clear()
         b = datetime.datetime.now()
         print(b-a)
