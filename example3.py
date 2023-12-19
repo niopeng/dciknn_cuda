976f1aef1d409b3f37680435f57212068331d388
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-12-19 15:03:19 -0800

    Update example3.py

--- example3.py
+++ example3.py
@@ -92,9 +92,9 @@ def main():
         dci_db.add(data)
         # Query
         indices, dists = dci_db.query(query, num_neighbours, num_outer_iterations)
-        #torch.set_printoptions(threshold=10000)
-        #print("Nearest Indices:", indices)
-        #print("Indices Distances:", dists)
+        torch.set_printoptions(threshold=10000)
+        print("Nearest Indices:", indices)
+        print("Indices Distances:", dists)
         dci_db.clear()
         b = datetime.datetime.now()
         print(b-a)
